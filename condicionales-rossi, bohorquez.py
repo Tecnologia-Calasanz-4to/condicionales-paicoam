5586cb6936cd433d557db6de0f7568dd3a1b78c7
paicoam <rossisantiago36@gmail.com>
2026-05-06 08:36:33 -0300

    Add files via upload

---
+++ condicionales-rossi, bohorquez.py
@@ -0,0 +1,7 @@
+temperatura=int(input("ingresa la temperatura"))
+if temperatura>25:
+    print ("hace caloron ")
+elif temperatura<10:
+    print ("hacxe frio")
+else:
+    print ("esta lindo jijij")
